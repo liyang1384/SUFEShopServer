12c4745f28cbdaaa46eb089a95a8e642acf77c22
cyp126 <240231247@qq.com>
2021-01-06 23:05:54 +0800

    urls修改

--- OrderService/urls.py
+++ OrderService/urls.py
@@ -1,11 +1,10 @@
 from rest_framework import routers
 from django.urls import include, path
-from OrderService.views import OrderDetail,BoughtOrderList,SoldOrderList,BuyerReviewDetail,SellerReviewDetail,PayOrderDetail,GenerateOrderDetail
+from OrderService.views import OrderDetail,OrderList,BuyerReviewDetail,SellerReviewDetail,PayOrderDetail,GenerateOrderDetail
 
 urlpatterns = [
     path('orderdetail/',OrderDetail.as_view()),
-    path('boughtorderlist/',BoughtOrderList.as_view()),
-    path('soldorderlist/',SoldOrderList.as_view()),
+    path('orderlist/',OrderList.as_view()),
     path('buyerreviewdetail/',BuyerReviewDetail.as_view()),
     path('sellerreviewdetail/',SellerReviewDetail.as_view()),
     path('payorderdetail/',PayOrderDetail.as_view()),
